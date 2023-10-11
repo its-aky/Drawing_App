c69c7c3da5dbb9f9464647424e5fffdd18bbcef9
its-aky <kumaramityadav230@gmail.com>
2023-10-11 10:35:03 +0530

    creating the view which will be used for drawing with parameters

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,9 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context:Context,attrs: AttributeSet) : View(context,attrs) {
+
+}
