265a2260237496846d43b446dd0043df0f0f59a3
its-aky <kumaramityadav230@gmail.com>
2023-10-11 10:39:26 +0530

    Revert "creating the view which will be used for drawing with parameters"
    This reverts commit c69c7c3da5dbb9f9464647424e5fffdd18bbcef9.

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.drawingapp
-
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-
-class DrawingView(context:Context,attrs: AttributeSet) : View(context,attrs) {
-
-}
