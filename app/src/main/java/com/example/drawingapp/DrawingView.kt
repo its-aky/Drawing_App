214c7c4b6fe36304198345a2fe4e128fe9824963
its-aky <kumaramityadav230@gmail.com>
2023-11-01 18:14:26 +0530

    added the required vars

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,9 +1,30 @@
 package com.example.drawingapp
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
-class DrawingView(context:Context,attrs: AttributeSet) : View(context,attrs) {
 
+class DrawingView(context:Context,attrs: AttributeSet) : View(context,attrs) {
+    private var mDrawPath:CustomPath?=null
+    private var mCanvasBitmap: Bitmap?=null
+    private var mDrawPaint: Paint?=null
+    private var mCanvasPaint: Paint?=null
+    private var mBrushSize: Float?=0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas?=null
+
+
+
+
+
+
+    internal inner class CustomPath(var color:Int, var brushThickness:Float):android.graphics.Path() {
+        //read about path by ctrl+click
+
+    }
 }
