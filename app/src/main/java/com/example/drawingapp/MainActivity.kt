ae8fb2f887fb3f515ffac70ef6b3e70e77f8e99f
its-aky <kumaramityadav230@gmail.com>
2023-11-05 00:06:20 +0530

    Adding Color pallete and creating xml files and updating the values in colors

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -5,14 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView:DrawingView?=null
+    private var mImageButtonCurrentPaint:ImageButton?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView=findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors=findViewById<LinearLayout>(R.id.ll_paint_colors)
+        //mImageButtonCurrentPaint = linearLayoutPaintColors[2] as ImageButton: This line retrieves the third child view (at index 2) of the linearLayoutPaintColors and casts it to an ImageButton. The assumption here is that the third child is an ImageButton.
+        //
+        //mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_pressed)): This line sets the image drawable of the mImageButtonCurrentPaint. It loads the drawable from the R.drawable.pallet_pressed resource using ContextCompat.getDrawable and sets it as the new drawable for the ImageButton. This action likely changes the appearance of the ImageButton to represent that it's "pressed" or active.
+        mImageButtonCurrentPaint=linearLayoutPaintColors[2] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
 
         val ib_brush:ImageButton=findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
