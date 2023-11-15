095a06c770d8e2c67e343dca33e20b57bffabac3
its-aky <kumaramityadav230@gmail.com>
2023-11-15 13:25:18 +0530

    Saving an Image on The Phone using Coroutines and OutPutStreams

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.drawingapp
 import android.Manifest
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -17,6 +21,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import android.widget.*
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -108,6 +120,33 @@ class MainActivity : AppCompatActivity() {
         ibRedo.setOnClickListener {
             drawingView?.onClickRedo()
         }
+
+        val ibSave: ImageButton = findViewById(R.id.ib_save)
+
+        ibSave.setOnClickListener {
+            //check if permission is allowed
+            if (isReadStorageAllowed()){
+                //launch a coroutine block
+                lifecycleScope.launch{
+                    //reference the frame layout
+                    val flDrawingView:FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                    //Save the image to the device
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        //Getting the permission status
+        // Here the checkSelfPermission is
+
+        val result = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        //If permission is granted returning true and If permission is not granted returning false
+        return result == PackageManager.PERMISSION_GRANTED
     }
 
     //Creating the opening dialog for the brush size selector
@@ -206,6 +245,118 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    //300->Coroutines -uses and implementation
+
+    // TODO(Getting and bitmap Exporting the image to your phone storage.)
+    /**
+     * Create bitmap from view and returns it
+     */
+    private fun getBitmapFromView(view: View): Bitmap {
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the specified width and height
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+        //Get the view's background
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        }
+        // draw the view on the canvas
+        view.draw(canvas)
+        //return the bitmap
+        return returnedBitmap
+    }
+
+    // TODO(Step 2 : A method to save the image.)
+
+    //This Kotlin function, saveBitmapFile, is designed to save a Bitmap image to a file. It does the following:
+    //
+    //Coroutine Usage: It is a suspend function, indicating that it can be called from a coroutine. The withContext(Dispatchers.IO) block ensures that the following code runs in the I/O dispatcher to perform disk I/O operations asynchronously.
+    //
+    //Bitmap Compression: The function compresses the input Bitmap using the PNG format with a quality level of 90%. The compressed data is written to a byte array output stream.
+    //
+    //File Generation: It creates a File in the external cache directory with a filename based on the current timestamp. The filename is constructed as "DrawingApp_" followed by the current time in seconds.
+    //
+    //File Output Stream: It writes the compressed byte array to a FileOutputStream associated with the created File. After writing, it closes the output stream.
+    //
+    //Result Handling: The absolute path of the saved file is stored in the result variable. If the file is saved successfully, a Toast message is displayed on the UI thread indicating the success; otherwise, an error message is shown.
+    //
+    //Exception Handling: Any exceptions that occur during the process are caught, and the result is set to an empty string.
+    //
+    //UI Interaction: The runOnUiThread block is used to display a Toast message on the UI thread based on the result of the file-saving operation.
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?):String{
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (mBitmap != null) {
+
+                try {
+                    val bytes = ByteArrayOutputStream() // Creates a new byte array output stream.
+                    // The buffer capacity is initially 32 bytes, though its size increases if necessary.
+
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    /**
+                     * Write a compressed version of the bitmap to the specified outputstream.
+                     * If this returns true, the bitmap can be reconstructed by passing a
+                     * corresponding inputstream to BitmapFactory.decodeStream(). Note: not
+                     * all Formats support all bitmap configs directly, so it is possible that
+                     * the returned bitmap from BitmapFactory could be in a different bitdepth,
+                     * and/or may have lost per-pixel alpha (e.g. JPEG only supports opaque
+                     * pixels).
+                     *
+                     * @param format   The format of the compressed image
+                     * @param quality  Hint to the compressor, 0-100. 0 meaning compress for
+                     *                 small size, 100 meaning compress for max quality. Some
+                     *                 formats, like PNG which is lossless, will ignore the
+                     *                 quality setting
+                     * @param stream   The outputstream to write the compressed data.
+                     * @return true if successfully compressed to the specified stream.
+                     */
+
+                    val f = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator + "DrawingApp_" + System.currentTimeMillis() / 1000 + ".jpg"
+                    )
+                    // Here the Environment : Provides access to environment variables.
+                    // getExternalStorageDirectory : returns the primary shared/external storage directory.
+                    // absoluteFile : Returns the absolute form of this abstract pathname.
+                    // File.separator : The system-dependent default name-separator character. This string contains a single character.
+
+                    val fo =
+                        FileOutputStream(f) // Creates a file output stream to write to the file represented by the specified object.
+                    fo.write(bytes.toByteArray()) // Writes bytes from the specified byte array to this file output stream.
+                    fo.close() // Closes this file output stream and releases any system resources associated with this stream. This file output stream may no longer be used for writing bytes.
+                    result = f.absolutePath // The file absolute path is return as a result.
+                    //We switch from io to ui thread to show a toast
+                    runOnUiThread {
+                        if (!result.isEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully :$result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
 
